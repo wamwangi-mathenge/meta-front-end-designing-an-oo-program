7da9eeb4f3fce2c2dcd53ec4162b5f889907c859
Brian <wamwangimathenge@gmail.com>
2023-08-10 15:54:04 +0300

    Design an OO Program
    This commit designs a skeleton OO program with the requirements written
    as comments in each class

---
+++ script.js
@@ -0,0 +1,31 @@
+// Designing an Object-Oriented Program
+
+class Animal {
+    // --- code here ---
+    // constructor: color, energy
+}
+
+class Cat extends Animal {
+    // --- code here ---
+    // constructor: color, energy, sound, canClimbHigh, canClimbTrees
+}
+
+class Bird extends Animal {
+    // --- code here ---
+    // constructor: color, energy, sound, canFly
+}
+
+class HouseCat extends Cat {
+    // --- code here ---
+    // constructor: houseCatSound, color, energy, sound, canClimbHigh, canClimbTrees
+}
+
+class Tiger extends Cat {
+    // --- code here ---
+    // constructor: tigerSound, color, energy, sound, canClimbHigh, canClimbTrees
+}
+
+class Parrot extends Bird {
+    // --- code here ---
+    // constructor: canTalk, color, energy, sound, canFly
+}
